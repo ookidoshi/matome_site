61088810d213c384feb8788656dde81adfdd362f
ookidoshi <okidookido@gmail.com>
2020-09-25 10:40:57 +0900

    add bouncing
    add bouncing action

---
+++ main.js
@@ -0,0 +1,10 @@
+let daytime = document.querySelector(".daytime");
+let jin = document.querySelector(".jin");
+
+addEventListener("keypress", function (x) {
+
+  if (x.which === 13) {
+    daytime.classList.toggle("moving");
+    jin.classList.toggle("bouncing");
+  }
+})
