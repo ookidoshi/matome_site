0f72e3c827771f07b4e3d2ae1ea62989a79a4ae2
ookidoshi <okidookido@gmail.com>
2020-09-28 09:05:07 +0900

    javascript_test
    javascript_test

--- main.js
+++ main.js
@@ -8,3 +8,13 @@
 //     jin.classList.toggle("bouncing");
 //   }
 // })
+
+
+function clickTest() {
+  target = document.getElementById("anime_test");
+  if (target.className == null || target.className == "") {
+    target.className = "active";
+  } else {
+    target.className = "";
+  }
+}
