fb4ffd6ef21b3f05d82eb5cc3c88fbf4e64c0a15
ookidoshi <okidookido@gmail.com>
2020-10-02 11:41:00 +0900

    js_delete

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-// let daytime = document.querySelector(".daytime");
-// let jin = document.querySelector(".jin");
-
-// addEventListener("keypress", function (x) {
-
-//   if (x.which === 13) {
-//     daytime.classList.toggle("moving");
-//     jin.classList.toggle("bouncing");
-//   }
-// })
-
-
-
 function clickTest() {
   target = document.getElementById("anime_test");
   if (target.className == null || target.className == "") {
